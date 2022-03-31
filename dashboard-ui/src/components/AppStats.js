44429db12eac57e3f5e03474096577aaac641246
98-percent-Milk <mjantsan@my.bcit.ca>
2022-03-31 15:21:33 -0700

    changed route port into path name

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -6,52 +6,52 @@ export default function AppStats() {
     const [stats, setStats] = useState({});
     const [error, setError] = useState(null)
 
-	const getStats = () => {
-	
-        fetch(`http://restapi.eastus2.cloudapp.azure.com:8100/stats`)
+    const getStats = () => {
+
+        fetch(`http://restapi.eastus2.cloudapp.azure.com/processing/stats`)
             .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Stats")
+            .then((result) => {
+                console.log("Received Stats")
                 setStats(result);
                 setIsLoaded(true);
-            },(error) =>{
+            }, (error) => {
                 setError(error)
                 setIsLoaded(true);
             })
     }
     useEffect(() => {
-		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
-		return() => clearInterval(interval);
+        const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+        return () => clearInterval(interval);
     }, [getStats]);
 
-    if (error){
+    if (error) {
         return (<div className={"error"}>Error found when fetching from API</div>)
-    } else if (isLoaded === false){
-        return(<div>Loading...</div>)
-    } else if (isLoaded === true){
-        return(
+    } else if (isLoaded === false) {
+        return (<div>Loading...</div>)
+    } else if (isLoaded === true) {
+        return (
             <div>
                 <h1>Latest Stats</h1>
                 <table className={"StatsTable"}>
-					<tbody>
-						<tr>
-							<th>Employee Resume</th>
-							<th>Job Description</th>
-						</tr>
-						<tr>
-							<td># Number of employees: {stats['num_employees']}</td>
-						</tr>
-						<tr>
-							<td># Popular field: {stats['popular_field']}</td>
-							<td colspan="2">Unpopular field: {stats['unpopular_field']}</td>
-						</tr>
-						<tr>
-							<td colspan="2">Company in need of hiring: {stats['desp_employer']}</td>
-						</tr>
-						<tr>
-							<td colspan="2">Company least need of hiring: {stats['least_desp_employer']}</td>
-						</tr>
-					</tbody>
+                    <tbody>
+                        <tr>
+                            <th>Employee Resume</th>
+                            <th>Job Description</th>
+                        </tr>
+                        <tr>
+                            <td># Number of employees: {stats['num_employees']}</td>
+                        </tr>
+                        <tr>
+                            <td># Popular field: {stats['popular_field']}</td>
+                            <td colspan="2">Unpopular field: {stats['unpopular_field']}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Company in need of hiring: {stats['desp_employer']}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Company least need of hiring: {stats['least_desp_employer']}</td>
+                        </tr>
+                    </tbody>
                 </table>
                 <h3>Last Updated: {stats['last_updated']}</h3>
 
--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -8,7 +8,7 @@ export default function EndpointAudit(props) {
     const [index, setIndex] = useState(null);
     const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
     const getAudit = () => {
-        fetch(`http://restapi.eastus2.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://restapi.eastus2.cloudapp.azure.com/audit_log/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result) => {
                 console.log("Received Audit Results for " + props.endpoint)
